d94090a2a3173e5f79f84d05335b4cfefcd6452f
damir_ja <zs.keda@gmail.com>
2017-04-17 19:28:16 +0500

    spinner add

---
+++ AkademAndroidMobile/AkademAndroidMobile/Resources/fragments/TimePickerFragment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace AkademAndroidMobile.Resources.fragments
+{
+    public class TimePickerFragment : DialogFragment, TimePickerDialog.IOnTimeSetListener
+    {
+        // TAG can be any string of your choice.
+        public static readonly string TAG = "X:" + typeof(TimePickerFragment).Name.ToUpper();
+
+        Action<DateTime> _timeSelectedHandler = delegate { };
+
+        public static TimePickerFragment NewInstance(Action<DateTime> onTimeSelected)
+        {
+            TimePickerFragment frag = new TimePickerFragment();
+            frag._timeSelectedHandler = onTimeSelected;
+            return frag;
+        }
+
+        public override Dialog OnCreateDialog(Bundle savedInstanceState)
+        {
+            DateTime currently = DateTime.Now;
+            TimePickerDialog dialog = new TimePickerDialog(Activity, this, currently.Hour, currently.Minute, true);
+            return dialog;
+        }
+
+        public void OnTimeSet(TimePicker view, int hourOfDay, int minute)
+        {
+            var today = DateTime.Today;
+            DateTime selectedTime = new DateTime(today.Year, today.Month, today.Day, hourOfDay, minute, 0);
+            Log.Debug(TAG, selectedTime.ToLongDateString());
+            _timeSelectedHandler(selectedTime);
+        }
+    }
+}
