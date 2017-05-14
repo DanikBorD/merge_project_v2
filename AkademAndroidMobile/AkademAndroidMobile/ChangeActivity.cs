4c785be438911c7a7752b3eeb3c50d04d08b2ef1
borisenko <d.borisenko@wellsoft.pro>
2017-05-14 14:16:38 +0500

    Last

--- AkademAndroidMobile/AkademAndroidMobile/ChangeActivity.cs
+++ AkademAndroidMobile/AkademAndroidMobile/ChangeActivity.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Graphics;
 
 namespace AkademAndroidMobile
 {
@@ -19,13 +20,21 @@ namespace AkademAndroidMobile
         TextView RoomTemp;
         TextView ConditionalTemp;
         TextView HeaterTemp;
-        Button RoomTempUp;
-        Button RoomTempDown;
-        Button ConditionalTempUp;
-        Button ConditionalTempDown;
-        Button HeaterTempUp;
-        Button HeaterTempDown;
-        Button Accept;
+        ImageButton RoomTempUp;
+        ImageButton RoomTempDown;
+        ImageButton ConditionalTempUp;
+        ImageButton ConditionalTempDown;
+        ImageButton HeaterTempUp;
+        ImageButton HeaterTempDown;
+        ImageButton Accept;
+        LinearLayout roomLine;
+        LinearLayout condLine1;
+        LinearLayout condLine2;
+        LinearLayout heatLine1;
+        LinearLayout heatLine2;
+        ToggleButton condToggleBtn;
+        ToggleButton heatToggleBtn;
+
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -37,13 +46,20 @@ namespace AkademAndroidMobile
             RoomTemp = FindViewById<TextView>(Resource.Id.RoomTemperatureTextView);
             ConditionalTemp = FindViewById<TextView>(Resource.Id.ConditionalTemperatureTextView);
             HeaterTemp = FindViewById<TextView>(Resource.Id.HeaterTemperatureTextView);
-            RoomTempUp = FindViewById<Button>(Resource.Id.UpRoomTemperatureButton);
-            RoomTempDown = FindViewById<Button>(Resource.Id.DownRoomTemperatureButton);
-            ConditionalTempUp = FindViewById<Button>(Resource.Id.UpConditionalTemperatureButton);
-            ConditionalTempDown = FindViewById<Button>(Resource.Id.DownConditionalTemperatureButton);
-            HeaterTempUp = FindViewById<Button>(Resource.Id.UpHeaterTemperatureButton);
-            HeaterTempDown = FindViewById<Button>(Resource.Id.DownHeaterTemperatureButton);
-            Accept = FindViewById<Button>(Resource.Id.AcceptBtn);
+            RoomTempUp = FindViewById<ImageButton>(Resource.Id.UpRoomTemperatureButton);
+            RoomTempDown = FindViewById<ImageButton>(Resource.Id.DownRoomTemperatureButton);
+            ConditionalTempUp = FindViewById<ImageButton>(Resource.Id.UpConditionalTemperatureButton);
+            ConditionalTempDown = FindViewById<ImageButton>(Resource.Id.DownConditionalTemperatureButton);
+            HeaterTempUp = FindViewById<ImageButton>(Resource.Id.UpHeaterTemperatureButton);
+            HeaterTempDown = FindViewById<ImageButton>(Resource.Id.DownHeaterTemperatureButton);
+            Accept = FindViewById<ImageButton>(Resource.Id.AcceptBtn);
+            roomLine = FindViewById<LinearLayout>(Resource.Id.roomLine);
+            condLine1 = FindViewById<LinearLayout>(Resource.Id.condLine1);
+            condLine2 = FindViewById<LinearLayout>(Resource.Id.condLine2);
+            heatLine1 = FindViewById<LinearLayout>(Resource.Id.heatLine1);
+            heatLine2 = FindViewById<LinearLayout>(Resource.Id.heatLine2);
+            condToggleBtn = FindViewById<ToggleButton>(Resource.Id.ConditionalToggleButton);
+            heatToggleBtn = FindViewById<ToggleButton>(Resource.Id.HeaterToggleButton);
 
             RoomTempUp.Click += RoomTempUp_Click;
             RoomTempDown.Click += RoomTempDown_Click;
@@ -55,8 +71,47 @@ namespace AkademAndroidMobile
             HeaterTempDown.Click += HeaterTempDown_Click;
 
             Accept.Click += Accept_Click;
+
+            IsAuto.CheckedChange += IsAuto_CheckedChanged;
+
+            if(IsAuto.Checked)
+            {
+                condLine1.SetBackgroundColor(Color.Gray);
+                condLine2.SetBackgroundColor(Color.Gray);
+
+                heatLine1.SetBackgroundColor(Color.Gray);
+                heatLine2.SetBackgroundColor(Color.Gray);
+            }
+            else
+            {
+                condLine1.SetBackgroundColor(Color.Green);
+                condLine2.SetBackgroundColor(Color.Green);
+
+                heatLine1.SetBackgroundColor(Color.Green);
+                heatLine2.SetBackgroundColor(Color.Green);
+            }
         }
 
+
+        private void IsAuto_CheckedChanged(object sender, EventArgs e)
+        {
+            if(!IsAuto.Checked)
+            { 
+                condLine1.SetBackgroundColor(Color.Green);
+                condLine2.SetBackgroundColor(Color.Green);
+
+                heatLine1.SetBackgroundColor(Color.Green);
+                heatLine2.SetBackgroundColor(Color.Green);
+            }
+            else
+            {
+                condLine1.SetBackgroundColor(Color.Gray);
+                condLine2.SetBackgroundColor(Color.Gray);
+
+                heatLine1.SetBackgroundColor(Color.Gray);
+                heatLine2.SetBackgroundColor(Color.Gray);
+            }
+        }
 
         private void Accept_Click(object sender, EventArgs e)
         {
@@ -94,14 +149,14 @@ namespace AkademAndroidMobile
 
         private void HeaterTempDown_Click(object sender, EventArgs e)
         {
-            int val = Convert.ToUInt16(ConditionalTemp.Text) - 1;
-            ConditionalTemp.Text = val.ToString();
+            int val = Convert.ToUInt16(HeaterTemp.Text) - 1;
+            HeaterTemp.Text = val.ToString();
         }
 
         private void HeaterTempUp_Click(object sender, EventArgs e)
         {
-            int val = Convert.ToUInt16(ConditionalTemp.Text) + 1;
-            ConditionalTemp.Text = val.ToString();
+            int val = Convert.ToUInt16(HeaterTemp.Text) + 1;
+            HeaterTemp.Text = val.ToString();
         }
     }
 }
